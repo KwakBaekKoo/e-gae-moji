00fce8264f26412efe13c60817b6f06cc525df56
chrisais9 <chrisais9@naver.com>
2023-11-27 11:13:52 +0900

    (fix) send and receive message

--- sendMessage.py
+++ sendMessage.py
@@ -3,17 +3,19 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 
+
 class SendMessageWidget(QLineEdit):
 
-    def __init__(self):
+    def __init__(self, send):
         super().__init__()
         self.setFixedWidth(300)
+        self.send = send
 
     def keyPressEvent(self, e):
-        #엔터 누르면 서버로 메시지 전송
+        # 엔터 누르면 서버로 메시지 전송
         if e.key() == Qt.Key_Return:
             print('send message to server')
+            self.send(self.text())
             self.clear()
         else:
             super().keyPressEvent(e)
-
