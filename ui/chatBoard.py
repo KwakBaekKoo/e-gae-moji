40eabc860e83f75b5dfaff1ade05b25741a8a50e
chrisais9 <chrisais9@naver.com>
2023-12-01 15:13:08 +0900

    (add) feed answer to host / update username on chat

--- ui/chatBoard.py
+++ ui/chatBoard.py
@@ -3,15 +3,15 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 
+
 class ChatBoardWidget(QListWidget):
 
-    def __init__(self, userName):
+    def __init__(self):
         super().__init__()
-        self.userName = userName  
         self.setFixedWidth(300)
         self.addItem('Welcome and Enjoy!')
 
-    def addMessage(self, message): # 서버로부터 메시지를 받아서 채팅창에 추가
-        self.addItem('%s: %s' %(self.userName, message))
-        QTimer.singleShot(0, lambda: self.verticalScrollBar().setValue(self.verticalScrollBar().maximum())) # 스크롤을 제일 아래로 내리기
-    
+    def addMessage(self, user, message):  # 서버로부터 메시지를 받아서 채팅창에 추가
+        self.addItem('%s: %s' % (user, message))
+        QTimer.singleShot(0, lambda: self.verticalScrollBar().setValue(
+            self.verticalScrollBar().maximum()))  # 스크롤을 제일 아래로 내리기
